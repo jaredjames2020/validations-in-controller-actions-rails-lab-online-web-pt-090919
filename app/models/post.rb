231ae08e4b75439fb328f4fd5a8fa146adcd1367
Jared S <jaredjames2020@gmail.com>
2020-02-13 20:22:44 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   
   validates :title, presence: true
   validates :category, inclusion: { in: ["Fiction","Non-Fiction"], message: "%{value} is not a valid category." }
-  validates :content, length {
+  validates :content, length: {minimum
   
 end
