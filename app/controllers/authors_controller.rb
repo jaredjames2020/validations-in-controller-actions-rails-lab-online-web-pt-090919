891e59ec0ac9a102da92d8476a15b836d09a5102
Jared S <jaredjames2020@gmail.com>
2020-02-13 20:40:03 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,8 +7,9 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
+    binding.pry
     if @author.valid?
+      @author = Author.create!(author_params)
       redirect_to author_path(@author)
     else
       redirect_to author_path
